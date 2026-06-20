3d3e1ca06d848576565d8e5f3579b8ee57f8a0d1
Yohannes-Ade <yohannesade21@gmail.com>
2026-06-21 01:08:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: './' makes all import paths relative so it works perfectly on GitHub Pages subdirectories
+  base: './', 
   plugins: [
     react()
   ],
