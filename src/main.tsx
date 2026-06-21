19d1c9d52079121028a517742c31e3d2f3e6bc11
Yohannes-Ade <yohannesade21@gmail.com>
2026-06-21 21:17:42 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App' // Changed from './App.tsx' to prevent strict bundler CI compilation errors
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
